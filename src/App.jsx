a1d3dfe7d16c6a762feebde3577ccbdb0082ed8c
Juan Ceballos <juandavi789@hotmail.com>
2023-08-27 18:47:04 -0500

    Structure of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import Info from './components/Info';
+import About from './components/About';
+import Interests from './components/Interests';
+import Footer from './components/Footer';
 import './App.css';
 
 const App = () => {
   return (
     <>
-      <h1>Hello world</h1>
+      <Info />
+      <About />
+      <Interests />
+      <Footer />
     </>
   );
 };
