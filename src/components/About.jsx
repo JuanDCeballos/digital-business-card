9107007bf5764d205a4bec33ed0ffe9d2c64b5e8
Juan Ceballos <juandavi789@hotmail.com>
2023-08-29 22:37:59 -0500

    Add some classes to be able to modify em in css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,8 @@
 const About = () => {
   return (
-    <div className="about-container">
-      <h3 className="about-container-title title">About</h3>
-      <p className="about-container-text text">
+    <div className='about-container'>
+      <h3 className='about-container-title title subtitle'>About</h3>
+      <p className='about-container-text text'>
         I am a frontend developer with a particular interest in making things
         simple and automating daily tasks. I try to keep up with security and
         best practices, and am always looking for new things to learn.
